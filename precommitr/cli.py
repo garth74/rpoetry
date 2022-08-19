e659cd286932378a1cf0f66bd82ddf69307f3fcb
Garrett Shipley <garrett.shipley7+github@gmail.com>
2022-08-19 08:32:45 -0400

    Added load_all and check to cli

--- precommitr/cli.py
+++ precommitr/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import typing as t
 from pathlib import Path
 
 import typer
@@ -10,9 +11,15 @@ from .utils import check_installed, run_in_R
 app = typer.Typer(name="precommitr")
 
 
+def echo_lines(lines: t.Iterable[str]):
+    """Echo lines of a command run in R."""
+    for line in lines:
+        typer.echo(line)
+
+
 @app.callback()
 def main():
-    """precommitR is essentially a commandline tool for devtools."""
+    """precommitR - the cli for devtools."""
     if not check_installed("devtools"):
         typer.echo("devtools is not installed.")
         raise typer.Abort(1)
@@ -20,7 +27,20 @@ def main():
 
 @app.command()
 def document(pkg: Path = typer.Option(".", resolve_path=True)):
-    """Calls the devtools::document() function on the current working directory."""
-    for line in run_in_R(f'devtools::document("{pkg.as_posix()}")'):
-        typer.echo(line)
+    """Calls devtools::document()."""
+    echo_lines(run_in_R(f'devtools::document("{pkg.as_posix()}")'))
+    typer.Exit(0)
+
+
+@app.command()
+def load_all(pkg: Path = typer.Option(".", resolve_path=True)):
+    """Calls devtools::load_all()."""
+    echo_lines(run_in_R(f'devtools::load_all("{pkg.as_posix()}")'))
+    typer.Exit(0)
+
+
+@app.command()
+def check(pkg: Path = typer.Option(".", resolve_path=True)):
+    """Calls devtools::check()."""
+    echo_lines(run_in_R(f'devtools::check("{pkg.as_posix()}")'))
     typer.Exit(0)
